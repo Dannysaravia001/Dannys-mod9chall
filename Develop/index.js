4fcc2e62894e4e89bf0454d14bf6ce4f1c99bc3d
unknown <dannysaravia100@gmail.com>
2023-12-04 22:11:01 -0500

    added inquirer input skeleton to js file

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,106 @@ const fs = require('fs');
 const inquirer = require('inquirer'); 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false; 
+            }
+        } 
+    },
+    {
+        type: '',
+        name: '',
+        message: '',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('');
+                return false; 
+            }
+        } 
+    },{
+        type: '',
+        name: '',
+        message: '',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('');
+                return false; 
+            }
+        } 
+    },{
+        type: '',
+        name: '',
+        message: '',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('');
+                return false; 
+            }
+        } 
+    },{
+        type: '',
+        name: '',
+        message: '',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('');
+                return false; 
+            }
+        } 
+    },{
+        type: '',
+        name: '',
+        message: '',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('');
+                return false; 
+            }
+        } 
+    },{
+        type: '',
+        name: '',
+        message: '',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('');
+                return false; 
+            }
+        } 
+    },{
+        type: '',
+        name: '',
+        message: '',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('');
+                return false; 
+            }
+        } 
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
