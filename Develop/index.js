f30036ac938382edd4891065c26f49062ac8540d
unknown <dannysaravia100@gmail.com>
2023-12-07 15:24:19 -0500

    added the rest of the questions to the inquirer function. getting started on the readme skeleton

--- Develop/index.js
+++ Develop/index.js
@@ -1,114 +1,111 @@
 // TODO: Include packages needed for this application
 const fs = require('fs'); 
 const inquirer = require('inquirer'); 
-
+const generatePage = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = () => [
+const questions = [
     {
         type: 'input',
-        name: 'github',
-        message: 'What is your GitHub username?',
+        name: 'name',
+        message: 'What is your Name?',
         validate: nameInput => {
             if (nameInput) {
                 return true;
             } else {
-                console.log('Please enter your GitHub username!');
+                console.log("Please enter your name!");
                 return false; 
             }
         } 
     },
     {
-        type: '',
-        name: '',
-        message: '',
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the name of your project?',
         validate: nameInput => {
             if (nameInput) {
                 return true;
             } else {
-                console.log('');
-                return false; 
-            }
-        } 
-    },{
-        type: '',
-        name: '',
-        message: '',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('');
-                return false; 
-            }
-        } 
-    },{
-        type: '',
-        name: '',
-        message: '',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('');
-                return false; 
-            }
-        } 
-    },{
-        type: '',
-        name: '',
-        message: '',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('');
-                return false; 
-            }
-        } 
-    },{
-        type: '',
-        name: '',
-        message: '',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('');
-                return false; 
-            }
-        } 
-    },{
-        type: '',
-        name: '',
-        message: '',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('');
-                return false; 
-            }
-        } 
-    },{
-        type: '',
-        name: '',
-        message: '',
-        validate: nameInput => {
-            if (nameInput) {
-                return true;
-            } else {
-                console.log('');
+                console.log('Please enter the name of your project!');
                 return false; 
             }
         } 
     },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'How would you use this app?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a usage description!');
+                return false; 
+            }
+        } 
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What licenses would you like to add in the project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a license!');
+                return false; 
+            }
+        } 
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'How do you install the project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter how to install!');
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the usages for the project?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter the usage information!');
+                return false; 
+            }
+        } 
+    }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.error(err);
+            return;
+        } 
+        console.log('README.md generated successfully!');
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then(answers => {
+            const readmeContent = generatePage(answers); // Assuming generatePage is an alias for generateMarkdown
+            writeToFile('README.md', readmeContent);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -14,6 +14,36 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
 `;
 }
 
